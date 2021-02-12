f7b4700aa8ac9856b4747749854f2b502a63d8e2
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-02-12 13:26:49 +0100

    vertical img, centered

--- src/App.js
+++ src/App.js
@@ -25,35 +25,18 @@ const App = () => {
       "imdbID": "tt1288499",
       "Type": "series",
       "Poster": "https://m.media-amazon.com/images/M/MV5BMTUwMjcyNzg5N15BMl5BanBnXkFtZTcwMDI0MzkwMw@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "La La Land",
-      "Year": "2016",
-      "imdbID": "tt3783958",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Going Down in LA-LA Land",
-      "Year": "2011",
-      "imdbID": "tt1599296",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTQwNjEzNTI0MV5BMl5BanBnXkFtZTcwMzI2MzY5Ng@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "La La Land",
-      "Year": "2010",
-      "imdbID": "tt1288499",
-      "Type": "series",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTUwMjcyNzg5N15BMl5BanBnXkFtZTcwMDI0MzkwMw@@._V1_SX300.jpg"
     }
   ])
   return (
-    <div className="container-fluid movie-app">
-      <div className="row">
-        <MovieList movies={movies}/>
-      </div>
-    </div>
+    <div className="container-fluid ">
+     
+
+
+      <MovieList movies={movies}/>
+</div>
+      
+     
+  
   );
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,22 +3,14 @@ import React from 'react';
 
 const MovieList = (props) => {
   return (
-    <div className="d-flex row img-container">
-      <>
-     
-        <i class="fas fa-chevron-left" onClick={() => {
-         window.scroll({right: 500, behavior: 'smooth'});
-        }}></i>
+    <div className="d-flex flex-wrap movielist-container">
+ 
           {props.movies.map((movie, index) => (
             <div className="m-3">
-              <img src={movie.Poster} className="h-100"></img>
+              <img src={movie.Poster} className="h-100 w-100"></img>
             </div>
           ))}
-        <i class="fas fa-chevron-right" onClick={() => {
-         window.scroll({left: 500, behavior: 'smooth'});
-        }}></i>
-      
-      </>
+    
     </div>
   )
 }
