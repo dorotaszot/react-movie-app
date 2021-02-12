e02e507fa919cef0ffaaf52fbb63be5ce5fb1413
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-02-12 15:04:11 +0100

    fetched data from url

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      "Title": "La La Land",
-      "Year": "2016",
-      "imdbID": "tt3783958",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Going Down in LA-LA Land",
-      "Year": "2011",
-      "imdbID": "tt1599296",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTQwNjEzNTI0MV5BMl5BanBnXkFtZTcwMzI2MzY5Ng@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "La La Land",
-      "Year": "2010",
-      "imdbID": "tt1288499",
-      "Type": "series",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTUwMjcyNzg5N15BMl5BanBnXkFtZTcwMDI0MzkwMw@@._V1_SX300.jpg"
+  const [movies, setMovies] = useState([]);
+
+    const getMoviesRequest = async () => {
+      const response = await fetch('http://www.omdbapi.com/?s=love&apikey=2821e511')
+      const data = await response.json();
+      console.log(data);
+      setMovies(data.Search);
     }
-  ])
+  
+    useEffect(() => {
+      getMoviesRequest();
+    }, []);
+
   return (
     <div className="container-fluid ">
-     
-
-
       <MovieList movies={movies}/>
-</div>
-      
-     
-  
+    </div>
   );
 }
 
