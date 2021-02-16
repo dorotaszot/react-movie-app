ec1381ac4038098fad4548b34c9c3c60585b8881
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-02-16 13:55:37 +0100

    favourites function added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AddToFavourites from './components/AddToFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
     const getMoviesRequest = async (searchValue) => {
@@ -23,14 +24,23 @@ const App = () => {
       getMoviesRequest(searchValue);
     }, [searchValue]);
 
+    const FavouriteClick = (movie) => {
+      const newFavouritesArr = [...favourites, movie];
+      setFavourites(newFavouritesArr);
+    }
+
   return (
-    <div className="container-fluid"> 
-      <div className="container d-flex align-items-center justify-content-between mt-3 mb-3">
+    <div className="container"> 
+      <div className="d-flex align-items-center justify-content-between mt-3 mb-3">
         <MovieListHeading heading='Movies'/>
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
-      <MovieList movies={movies} addToFavourites={AddToFavourites}/>
+        <MovieList movies={movies} addToFavourites={AddToFavourites} handleFavouriteClick={FavouriteClick}/>
       
+      <div className="mt-3 mb-3">
+        <MovieListHeading heading='Favourites' className="container"/>
+        <MovieList movies={favourites} addToFavourites={AddToFavourites} className="d-flex align-items-center justify-content-between container-fluid"/>
+      </div>
     </div>
   );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,12 +5,12 @@ const MovieList = (props) => {
   const AddToFavouritesComponent = props.addToFavourites;
   return (
 
-    <div className="d-flex flex-wrap movielist-container">
+    <div className="d-flex flex-wrap justify-content-center align-items-center container">
  
           {props.movies.map((movie, index) => (
-            <div className="m-3 img-container">
+            <div className="m-3">
               <img src={movie.Poster} className="h-100 w-100"></img>
-              <div className="overlay d-flex justify-content-center align-items-center p-3"><AddToFavouritesComponent /></div>
+              <div className="overlay d-flex justify-content-center align-items-center p-3" onClick={() => props.handleFavouriteClick(movie)}><AddToFavouritesComponent /></div>
             </div>
           ))}
     
